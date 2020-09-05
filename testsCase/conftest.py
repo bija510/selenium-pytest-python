7b4a9548aa6fc1db34ccc744e3d3ee793123d4e8
bija510 <error.zxz@gmail.com>
2020-09-04 23:22:07 -0400

    added Reporting & Excel File Data Driven

--- testsCase/conftest.py
+++ testsCase/conftest.py
@@ -25,5 +25,23 @@ def pytest_addoption(parser): # this will get the value from CLI / hooks
 def browser(request): # this will return the browser value from setup method
 	return request.config.getoption("--browser")
 
+########### pytest HTML Report ################
+
+# It is hook for Adding Environment info to HTML Report
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'letskodeit'
+    config._metadata['Module Name'] = 'Customers'
+    config._metadata['Tester'] = 'Bijay'
+
+# It is hook for delete/Modify Environment info to HTML Report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
+
+
 #py.test -v -s test_login.py --browser chrome
 #py.test -v -s test_login.py --browser firefox
+#py.test -v -s --html=../Reports/report.html test_login.py --browser chrome
+
+
--- testsCase/test_loginDDT.py
+++ testsCase/test_loginDDT.py
@@ -3,32 +3,22 @@ from selenium import webdriver
 import pytest
 from utilites.customLogger import logGen
 from utilites.readProperties import ReadConfig
+from utilites import XLUtils
+import  time
+
 
 class TestloginClass():
-	baseUrl = ReadConfig.getApplicationURL()
-	userName = ReadConfig.getUserName()
-	password = ReadConfig.getPassword()
+
+	##############################
+	# Reading Data from Excel File
+	###############################
+
+	path = "../testData/InputData.xlsx"
+	baseUrl = XLUtils.readData(path, "Sheet1", 2, 1)
+	userName = XLUtils.readData(path, "Sheet1", 3, 1)
+	password = XLUtils.readData(path, "Sheet1", 4, 1)
 
 	logger = logGen.loggen()
-
-	def test_homePageTitle(self, setup):
-		self.logger.info("----------TestloginClass---------")
-		self.logger.info("----------Verifying HomePage Title---------")
-		self.driver = setup
-		self.driver.get(self.baseUrl)
-		actTitle = self.driver.title
-
-		if actTitle =='OrangeHRM':
-			assert True
-			self.driver.close()
-			self.logger.info("----------Home page verification Passed---------")
-		else:
-			self.driver.save_screenshot(
-				"../Screenshot/" + "testHomepage1.png")
-			self.driver.close()
-			self.logger.error("----------Home page verification Failed---------")
-			assert False
-
 
 	def test_login(self, setup):
 		self.driver = setup
@@ -39,7 +29,7 @@ class TestloginClass():
 		self.lp.setPassword(self.password)
 		self.lp.clickLoginButton()
 		actTitle = self.driver.title
-
+		time.sleep(3)
 		if actTitle == 'OrangeHRM':
 			assert True
 			self.driver.close()
@@ -51,7 +41,14 @@ class TestloginClass():
 			self.logger.error("----------test_login page verification Failed---------")
 			assert False
 
-		#self.driver.close()
+	###############################
+	# Writing data to Excel file
+	#################################
+
+	XLUtils.writeData(path, "Sheet1", 3, 2, "Test_LoginDDT Pass")
+
+	# py.test -v -s test_loginDDT.py --browser chrome
+	#self.driver.close()
 
 """
 https://github.com/pavanoltraining/nopCommerceProject
@@ -59,7 +56,7 @@ https://github.com/pavanoltraining/nopCommerceProject
 # py.test -v -s test_login.py --browser chrome
 # py.test -v -s test_login.py --browser firefox
 # Run parallel max -3 ===>How many method we have just pass num
-# py.test -v -s -n=2 test_login.py --browser chrome
+# py.test -v -s test_loginDDT.py --browser chrome
 # To run Anything we have to Run from CMD or terminal ==r-click package -->
 # Open in terminal --> py.test
 # To run full with details--> py.test -v -s
